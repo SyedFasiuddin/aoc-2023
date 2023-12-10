83c2011f2e78fa8b85eafa713dff004ed4f00191
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2023-12-10 22:55:37 +0530

    day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 use std::{fs::File, io::Read};
 
-#[derive(Eq, PartialEq, Hash, Debug)]
+#[derive(Eq, PartialEq, Hash, Debug, Clone, Copy)]
 struct Node {
     name: [char; 3],
 }
@@ -39,6 +39,17 @@ fn main() {
     BBB = (AAA, ZZZ)
     ZZZ = (ZZZ, ZZZ)";
 
+    let _input = "LR
+
+    11A = (11B, XXX)
+    11B = (XXX, 11Z)
+    11Z = (11B, XXX)
+    22A = (22B, XXX)
+    22B = (22C, 22C)
+    22C = (22Z, 22Z)
+    22Z = (22B, 22B)
+    XXX = (XXX, XXX)";
+
     let mut input = String::new();
     let _ = File::open("inputs/8.txt")
         .unwrap()
@@ -60,24 +71,61 @@ fn main() {
         map.insert(node, (left, right));
     }
 
-    let mut curr_node = map.get(&Node::from("AAA")).unwrap();
-    let mut count: u64 = 0;
-
-    loop {
-        for direction in route.chars() {
-            match direction {
-                'L' => curr_node = map.get(&curr_node.0).unwrap(),
-                'R' => curr_node = map.get(&curr_node.1).unwrap(),
-                _ => unreachable!(),
-            }
-        }
-        if curr_node != map.get(&Node::from("ZZZ")).unwrap() {
-            count += route.len() as u64;
-        } else {
-            count += route.len() as u64;
-            break;
+    let mut start_nodes: Vec<Node> = Vec::new();
+    for (node, _) in &map {
+        if node.name[2] == 'A' {
+            start_nodes.push(node.clone());
         }
     }
 
-    println!("{count}");
+    let mut nums: Vec<u64> = Vec::new();
+    let mut count: u64 = 0;
+
+    for node in start_nodes.iter_mut() {
+        loop {
+            for direction in route.chars() {
+                let node_tmp = map.get(&node).unwrap();
+                match direction {
+                    'L' => *node = node_tmp.0,
+                    'R' => *node = node_tmp.1,
+                    _ => unreachable!(),
+                }
+            }
+            if node.name[2] == 'Z' {
+                count += route.len() as u64;
+                nums.push(count);
+                count = 0;
+                break;
+            } else {
+                count += route.len() as u64;
+            }
+        }
+    }
+
+    let mut l = nums[0];
+    for num2 in nums.iter().skip(1) {
+        l = lcm(l, *num2);
+    }
+    println!("{l}");
+}
+
+fn gcd(mut a: u64, mut b: u64) -> u64 {
+    if a == b {
+        return a;
+    }
+    if b > a {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    while b > 0 {
+        let temp = a;
+        a = b;
+        b = temp % b;
+    }
+    return a;
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    return a * (b / gcd(a, b));
 }
